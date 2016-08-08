ea5f6c3213cca09a2448ba8b1cd95bb6f4ea5c0c
crumpx <crumpx@gmail.com>
2016-08-08 17:24:13 -0400

    Initial Commit
    before "Observables"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
+import {ROUTER_DIRECTIVES} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [ ROUTER_DIRECTIVES],
+  providers: [HeroService]
+
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title: 'Heroes!!!';
+
+  ngOnInit(){
+
+  }
+
+  goBack(){
+    window.history.back();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import {appRouterProviders} from "./app/app.routes";
+import {HTTP_PROVIDERS} from "@angular/http";
+import { XHRBackend } from '@angular/http';
+import { InMemoryDataService } from './app/in-memory-data.service'
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  appRouterProviders,
+  HTTP_PROVIDERS,
+  { provide: XHRBackend, useClass: InMemoryBackendService},
+  { provide: SEED_DATA, useClass: InMemoryDataService}
+
+]);
